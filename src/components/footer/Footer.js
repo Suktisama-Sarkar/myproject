ccc366d860031c2aff95db845564228e022c397b
Suktisama <suktisama.sarkar@gmail.com>
2021-12-26 20:47:59 +0530

    footer added

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,8 +1,31 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './footerStyle.css';
+import Fbicon  from '../../assets/images/facebook.jpg';
+import Jdicon  from '../../assets/images/justdial.jpg';
 
 const Footer = () => {
     return(
-        <>Footer</>
+        <>
+        <footer>
+            <div className="container">
+                <div className='row'>
+                    <div className="col-md-12 text-center">
+                        <Link to="#"> <img src={Fbicon} alt="FB"/></Link>
+                        <Link to="#"> <img src={Jdicon} alt="FB"/></Link>
+                    </div>
+                </div>
+                <div className='row'>
+                    <div className="col-md-12 text-center">
+                        Copyright
+                    </div>
+                </div>
+            </div>
+            
+            
+            
+            </footer>
+        </>
     );
 }
 
